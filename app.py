91bf6d81070b0f98d252629131b488d31a7bc04e
Arun Dwivedi <arunkdwivedi@gmail.com>
2026-03-29 22:55:55 +0530

    gemini project added

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
-#demo edit
-#edit again
+from google import genai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client =genai.Client(api_key=os.getenv("API_KEY"))
+
+response=client.models.generate_content(model="gemini-2.5-flash", contents=" Explain python loops to a 10 year old in 100 words")
+
+print(response.text)
